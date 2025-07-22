9c363eaa0b8698bc83fa58bc43cd406267a2a653
cquinto <cristian.quinto0125@jala.university>
2025-07-22 19:51:19 -0300

    Add Main Card Game Simulation

--- act1-card-game/README.md
+++ act1-card-game/README.md
@@ -12,3 +12,5 @@ mvn clean install
 _Póker: ¿Cómo se Juega y Cuántas Cartas se Dan Para Hacer Escalera?_. (2024, agosto 20). Clarín. https://www.clarin.com/informacion-general/poker-juega-cuantas-cartas-dan-hacer-escalera_0_fEgrAGdWMZ.html?srsltid=AfmBOoqB7pEw4_1mboB-O-jnlsGbkVeVTmtJP2Nd1npmj8k0iT87eAhH
 
 _Random Shuffling of an Array_. (S/f). Stackoverflow.com. Retrieved on 22 July 2025 from https://stackoverflow.com/questions/1519736/random-shuffling-of-an-array/21454317#21454317
+
+_Clasificación de las Jugadas del Poker_. Poker.com.es. Retrieved on 22 July 2025 from https://www.poquer.com.es/ranking.html
--- act1-card-game/src/main/java/org/cquinto/MainApplication.java
+++ act1-card-game/src/main/java/org/cquinto/MainApplication.java
@@ -2,6 +2,7 @@ package org.cquinto;
 
 import org.cquinto.domain.Card;
 import org.cquinto.domain.Deck;
+import org.cquinto.domain.Value;
 
 /**
  * Cristian Ezequiel Quinto
@@ -15,7 +16,44 @@ public class MainApplication {
         Card computerCard = deck.getRandomCard();
         Card playerCard = deck.getRandomCard();
 
-        System.out.printf("Computer: %s, Player: %s", computerCard, playerCard);
+        System.out.printf("Computer: %s, Player: %s\n", computerCard, playerCard);
+
+        int computerValue = getCardValue(computerCard.getValue());
+        int playerValue = getCardValue(playerCard.getValue());
+
+        if (computerValue > playerValue) {
+            System.out.println("Perdiste");
+        } else if (computerValue < playerValue) {
+            System.out.println("Ganaste");
+        } else {
+            System.out.println("Empate");
+        }
     }
+
+    private static int getCardValue(Value value) {
+        int result;
+
+        String currentValue = value.getType();
+
+        switch (currentValue) {
+            case "J":
+                result = 11;
+                break;
+            case "Q":
+                result = 12;
+                break;
+            case "K":
+                result = 13;
+                break;
+            case "A":
+                result = 14;
+                break;
+            default:
+                result = Integer.parseInt(currentValue);
+        }
+
+        return result;
+    }
+
 
 }
