1997d03d7307aac6da19a6cc810500130d7724d4
cquinto <cristian.quinto0125@jala.university>
2025-07-22 10:19:56 -0300

    Add Value Enum Abstraction

---
+++ act1-card-game/src/main/java/org/cquinto/domain/Value.java
@@ -0,0 +1,18 @@
+package org.cquinto.domain;
+
+public enum Value {
+    TWO("2"), THREE("3"), FOUR("4"), FIVE("5"),
+    SIX("6"), SEVEN("7"), EIGHT("8"), NINE("9"),
+    TEN("10"), JACK("J"), QUEEN("Q"), KING("K"), AS("A");
+
+    private final String value;
+
+    Value(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
